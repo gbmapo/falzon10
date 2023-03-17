c1d7675244488f251c40caa7e6a473f96e7d39e7
gbmapo <gbmapo@gmail.com>
2023-03-17 21:03:42 +0100

    Improved mymovies

--- web/modules/custom/eric/src/Form/SeenMovies.php
+++ web/modules/custom/eric/src/Form/SeenMovies.php
@@ -279,6 +279,8 @@ class SeenMovies extends FormBase
             break;
           case 'comment':
             $comment = strtr($value, $transcod);
+            $comment = str_replace("{", "<i>", $value);
+            $comment = str_replace("}", "</i>", $review);
             $comment = str_replace(["\r\n", "\n", "\r"], '<br />', $comment);
             break;
           case 'country':
@@ -354,7 +356,7 @@ class SeenMovies extends FormBase
       switch ($this->step) {
         case '2T':
           $class = ($tvshow == 1) ? "TVShow" : "";
-          $html = '<div><h1 class="tttt' . $class . '"><img src="' . $image . '">' . $title . $trailer . '<span class="date">' . $date . '</span></h1><p class="justify">' . $review . '</p>' . ($signature == '' ? '' : '<p class="signature">' . $signature . '</p>') . '<hr></div>';
+          $html = '<div><h1 class="tttt' . $class . '">' . $title . $trailer . '<span class="date">' . $date . '</span></h1><p class="justify"><img src="' . $image . '">' . $review . '</p>' . ($signature == '' ? '' : '<p class="signature">' . $signature . '</p>') . '<hr></div>';
           break;
         case '2U':
           $html = '<div><h1 class="ulysse">' . $title . $trailer . '<span class="date">' . $date . '</span></h1><h2 class="justify">' . $comment . '</h2><p class="justify"><img class="imageulysse" src="' . $image . '">' . $review . '</p><p class="signature">' . $signature . '</p><h5>' . ($titleoriginal == '[] ' ? '' : $titleoriginal) . $country . $duration . $director . $actors . '</h5><p>' . $movietheater . '</p><hr></div>';
