a0839b30aaa4db234f172e158d0bb6e25ae52dcd
gbmapo <gbmapo@gmail.com>
2024-05-19 11:38:38 +0200

    Added search in ReadBooks.php.

--- web/modules/custom/eric/src/Form/ReadBooks.php
+++ web/modules/custom/eric/src/Form/ReadBooks.php
@@ -6,29 +6,36 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal;
 use Drupal\Core\Url;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\BeforeCommand;
+use Drupal\Core\Ajax\MessageCommand;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Drupal\Core\Ajax\ReplaceCommand;
 
 /**
  * Class ReadBooks.
  */
-class ReadBooks extends FormBase
-{
+class ReadBooks extends FormBase {
 
   protected $step = 1;
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId()
-  {
+  public function getFormId() {
     return 'read_books';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
     if ($this->step == 1) {
+
+      $form['#prefix'] = '<div id="form_step1">';
+      $form['#suffix'] = '</div>';
+
 
       if (Drupal::currentUser()->hasPermission('add books')) {
         $form['new'] = [
@@ -38,7 +45,7 @@ class ReadBooks extends FormBase
         ];
       }
 
-      $currentYear = (int)date('Y');
+      $currentYear = (int) date('Y');
       $options = [];
       for ($i = 2002; $i < $currentYear + 1; $i++) {
         $options[$i] = $i;
@@ -58,6 +65,35 @@ class ReadBooks extends FormBase
           'wrapper' => 'books',
         ],
       ];
+
+      $form['stringtosearch'] = [
+        '#prefix' => '<div id="searcharea">',
+        '#type' => 'textfield',
+        '#size' => 32,
+        '#default_value' => '',
+        '#placeholder' => t('Enter at least 3 characters'),
+        '#attributes' => ['id' => 'stringtosearch'],
+      ];
+      $form['submittosearch'] = [
+        '#type' => 'submit',
+        '#name' => 'search',
+        '#value' => t('Search'),
+        '#ajax' => [
+          'callback' => '::ajaxSearch',
+          'wrapper' => 'form_step1',
+          'progress' => [
+            'type' => 'throbber',
+            'message' => NULL,
+          ],
+        ],
+        '#suffix' => '</div>',
+        '#states' => [
+          'visible' => [
+            ':input[name="stringtosearch"]' => ['!value' => ''],
+          ],
+        ],
+      ];
+
 
       $form['books'] = [
         '#type' => 'container',
@@ -140,46 +176,103 @@ class ReadBooks extends FormBase
     return $form;
   }
 
-  public function yearCallback($form, FormStateInterface $form_state)
-  {
+  public function yearCallback($form, FormStateInterface $form_state) {
     return $form['books'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-//     foreach ($form_state->getValues() as $key => $value) {
-//     }
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    if ($this->step == 1) {
+      if ($form_state->getTriggeringElement()['#name'] == 'search') {
+        $values = $form_state->getValues();
+        if (mb_strlen($values['stringtosearch']) < 3) {
+          $form_state->setErrorByName('stringtosearch', $this->t('You must enter at least 3 characters.'),);
+        }
+        else {
+          $command = 'grep -hir "' . $values['stringtosearch'] . '" mybooks/index20*';
+          $output = shell_exec($command);
+          if ($output) {
+            $form_state->set('stringtosearch', $values['stringtosearch']);
+            $form_state->set('output', $output);
+          }
+          else {
+            $form_state->setErrorByName('stringtosearch', $this->t('« %stringtosearch » was not found.', [
+              '%stringtosearch' => $values['stringtosearch'],
+            ]),);
+          }
+        }
+      }
+    }
     parent::validateForm($form, $form_state);
+  }
+
+  public function ajaxSearch(array &$form, FormStateInterface $form_state) {
+
+    $response = new AjaxResponse();
+    $attachments['library'][] = 'core/drupal.dialog.ajax';
+    $response->setAttachments($attachments);
+
+    if ($form_state->hasAnyErrors()) {
+      $messages = Drupal::messenger()->deleteAll();
+      $response->addCommand(new ReplaceCommand('.message.message-error', ''));
+      $messages = [
+        '#theme' => 'status_messages',
+        '#message_list' => $messages,
+      ];
+      $response->addCommand(new BeforeCommand('#searcharea', $messages));
+    }
+    else {
+      $form['stringtosearch']['#value'] = '';
+      $response->addCommand(new ReplaceCommand(NULL, $form));
+      $content = '<head><base href="/mybooks/">';
+      $content .= $form_state->getStorage()['output'];
+      $content .= '</head>';
+      $title = t('Search for « %stringtosearch »', [
+        '%stringtosearch' => $form_state->getStorage()['stringtosearch'],
+      ]);
+      $dialog_options = [
+        'width' => '90%',
+      ];
+      $settings = [];
+      $response->addCommand(new OpenModalDialogCommand($title, $content, $dialog_options, $settings));
+    }
+    return $response;
+
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
 
     if ($this->step == 1) {
-      $form_state->setRebuild();
-      $this->step = 2;
+      if ($form_state->getTriggeringElement()['#name'] == 'search') {
+      }
+      else {
+        $form_state->setRebuild();
+        $this->step = 2;
+      }
     }
     else {
-
-      $transcod = get_html_translation_table(HTML_ENTITIES);
 
       foreach ($form_state->getValues() as $key => $value) {
 
         switch ($key) {
           case 'author':
-            $author = strtr($value, $transcod);
+            $author = $value;
             break;
           case 'backcover':
-            $backcover = strtr($value, $transcod);
+            $backcover = $value;
             $backcover = str_replace("{", "<i>", $value);
             $backcover = str_replace("}", "</i>", $backcover);
-            $backcover = str_replace(["\r\n", "\n", "\r"], '<br />', $backcover);
+            $backcover = str_replace([
+              "\r\n",
+              "\n",
+              "\r",
+            ], '<br />', $backcover);
             break;
           case 'cover':
             if ($value == "") {
@@ -187,13 +280,13 @@ class ReadBooks extends FormBase
             }
             else {
               $sNumber = strstr($value, "{");
-              $sName = strstr($value, "{", true);
-              if ($sNumber == false) {
+              $sName = strstr($value, "{", TRUE);
+              if ($sNumber == FALSE) {
                 $cover = '<A>C<IMG ALT="" SRC="images/' . $value . '.jpg"></A>';
               }
               else {
-                $iMin = (int)substr($sNumber, 1, 2);
-                $iMax = (int)substr($sNumber, 3, 2);
+                $iMin = (int) substr($sNumber, 1, 2);
+                $iMax = (int) substr($sNumber, 3, 2);
                 $iMax = ($iMax == 0) ? $iMin : $iMax;
                 $cover = '';
                 for ($i = $iMin; $i <= $iMax; $i++) {
@@ -204,23 +297,23 @@ class ReadBooks extends FormBase
             }
             break;
           case 'publication':
-            $publication = strtr($value, $transcod);
+            $publication = $value;
             break;
           case 'reading':
-            $reading = strtr($value, $transcod);
+            $reading = $value;
             break;
           case 'title':
-            $title = strtr($value, $transcod);
+            $title = $value;
             break;
           case 'titleoriginal':
-            $titleoriginal = $value ? strtr($value, $transcod) . '<br>' : '';
+            $titleoriginal = $value ? $value . '<br>' : '';
             break;
           default:
         }
       }
       $html = '<div><h1>' . $title . '</h1><p class="justify"><span class="floatleft">' . $author . '<br>' . $titleoriginal . $publication . '<br>' . $reading . '</span>' . $cover . $backcover . '</p></div>';
 
-      $currentYear = (int)date('Y');
+      $currentYear = (int) date('Y');
       $file = 'mybooks/index' . $currentYear . '.html';
 
       $content = file_get_contents($file);
